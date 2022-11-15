3d7ecd37830a57a9e04d0f23151ab245758ed6af
d.cooleshov <retmonhy1@gmail.com>
2022-11-15 22:09:33 +0700

    step 2

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
   for (let i = 0; i <= dots; i++) temp[i] = new Array(dots);
   let alfa = new Array(dots);
   let beta = new Array(dots);
+  let gamma = new Array(dots);
   let A = new Array(dots);
   let B = new Array(dots);
   let C = new Array(dots);
@@ -15,6 +16,7 @@
   const capacity = 700;
   const ro = 7500;
   const outerRadius = 1;
+  const innerRadius = 0.1;
   const temp_upper = 500;
   const temp_lower = 300;
   const kappa = lam / (capacity * ro);
@@ -22,7 +24,12 @@
   const tau = 0.01;
   const h_rad = outerRadius / dots;
   const h_fi = (2 * Math.PI) / dots;
-  const kurant = (tau * kappa) / (2 * pow(h_rad, 2));
+  const kurant = (tau * kappa) / 2;
+
+  //вспомогательные фукнции
+  function rad_calc(k: number) {
+    return innerRadius + h_rad * k;
+  }
 
   let time = 0;
   //начальные условия
@@ -43,17 +50,19 @@
     //расчет температуры по радиусу
     for (let i = 1; i <= dots - 1; i++) {
       for (let k = 1; k <= dots - 1; k++) {
-        const kur_fi = pow(pow(h_fi * k, 2), -1);
-        A[k] = kurant * (1 + pow(k, -1));
-        B[k] = kurant * (-2 - pow(k, -1)) - 1;
-        C[k] = kurant;
+        A[k] = kurant * (1 / pow(h_rad, 2) + 1 / (rad_calc[k] * h_rad));
+        B[k] = kurant * (-2 / pow(h_rad, 2) - 1 / (rad_calc[k] * h_rad)) - 1;
+        C[k] = kurant / pow(h_rad, 2);
         D[k] =
-          -temp[k][i + 1] * kurant * kur_fi + temp[k][i] * (2 * kurant * kur_fi + 1) - temp[k][i - 1] * kurant * kur_fi;
+          kurant *
+          (-temp[k][i + 1] / pow(rad_calc[k] * h_rad, 2) +
+            temp[k][i] * (2 / (rad_calc[k] * pow(h_fi, 2)) - 1 / kurant) -
+            temp[k][i - 1] / pow(rad_calc[k] * h_fi, 2));
       }
-      A[0] = 0; // 1
+      A[0] = 1;
       B[0] = -1;
       C[0] = 0;
-      D[0] = 300; // 0
+      D[0] = 0;
       A[dots] = -1;
       B[dots] = 1;
       C[dots] = 0;
@@ -62,7 +71,7 @@
       alfa[0] = -A[0] / B[0];
       beta[0] = D[0] / B[0];
 
-      for (let k = 1; k <= dots - 1; k++) {
+      for (let k = 1; k < dots; k++) {
         alfa[k] = -A[k] / (C[k] * alfa[k - 1] + B[k]);
         beta[k] = (D[k] - C[k] * beta[k - 1]) / (C[k] * alfa[k - 1] + B[k]);
       }
@@ -74,22 +83,32 @@
 
     //расчет температуры по углу
     for (let k = 1; k <= dots - 1; k++) {
-      for (let i = 1; i <= dots - 1; i++) {
-        const kur_fi = pow(pow(h_fi * k, 2), -1);
-        // if (i === 0 || i === dots / 2) {
-        //   A[i] = 1;
-        //   B[i] = -2;
-        //   C[i] = 1;
-        //   D[i] = 0;
-        //   continue;
-        // }
-        A[i] = kurant * kur_fi;
-        B[i] = -2 * kur_fi * kurant - 1;
-        C[i] = A[k];
+      const kur_fi = pow(rad_calc[k] * h_fi, 2);
+      //проверить все нижестоящие формулы.  У Князевой буквы перед функцией другие
+      A[1] = kurant / kur_fi;
+      B[1] = (-2 * kurant) / kur_fi - 1;
+      C[1] = A[1];
+      D[1] =
+        -1.0 * temp[k + 1][1] * (kurant / pow(h_rad, 2) + 1 / (rad_calc[k] * h_rad)) +
+        temp[k][1] * (kurant * (2 / pow(h_rad, 2) + 1 / (h_rad * rad_calc[k])) - 1) -
+        temp[k - 1][1] * (kurant / pow(h_rad, 2));
+
+      alfa[2] = -B[1] / C[1];
+      beta[2] = -D[1] / C[1];
+      gamma[2] = -A[1] / C[1];
+
+      for (let i = 2; i <= dots; i++) {
+        A[i] = kurant / kur_fi;
+        B[i] = (-2 * kurant) / kur_fi - 1;
+        C[i] = A[i];
         D[i] =
-          -temp[k + 1][i] * kurant * (1 + pow(k, -1)) -
-          temp[k][i] * (1 - kurant * (2 + pow(k, -1))) -
-          temp[k - 1][i] * kurant;
+          -1.0 * temp[k + 1][1] * (kurant / pow(h_rad, 2) + 1 / (rad_calc[k] * h_rad)) +
+          temp[k][1] * (kurant * (2 / pow(h_rad, 2) + 1 / (h_rad * rad_calc[k])) - 1) -
+          temp[k - 1][1] * (kurant / pow(h_rad, 2));
+
+        alfa[i] = B[i];
+        // beta[i] = ;
+        // gamma[i] = ;
 
         if (Math.abs(temp[k][i]) > 5000) {
           return console.log(
@@ -98,25 +117,6 @@
             temp[k][i]
           );
         }
-        // console.log("temp = ", temp[k - 1][i]);
-      }
-      A[0] = 1;
-      B[0] = -2;
-      C[0] = 1;
-      D[0] = 0;
-      A[dots] = 1;
-      B[dots] = -2;
-      C[dots] = 1;
-      D[dots] = 0;
-      alfa[0] = -A[0] / B[0];
-      beta[0] = D[0] / B[0];
-      for (let i = 1; i <= dots - 1; i++) {
-        alfa[i] = -A[i] / (C[i] * alfa[i - 1] + B[i]);
-        beta[i] = (D[i] - C[i] * beta[i - 1]) / (C[i] * alfa[i - 1] + B[i]);
-      }
-      temp[k][dots] = (D[dots] - C[dots] * beta[dots - 1]) / (C[dots] * alfa[dots - 1] + B[dots]);
-      for (let i = dots - 1; i >= 0; i--) {
-        temp[k][i] = temp[k][i + 1] * alfa[i] + beta[i];
       }
     }
   } while (time < 5);
